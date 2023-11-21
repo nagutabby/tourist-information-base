4a281c9de0f8e87546a61fdbf74946837b4aeb80
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-11-21 23:23:22 +0900

    本番環境でマーカー画像が表示されるようにする

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import type { Marker, MarkerOptions } from "leaflet";
   import "leaflet/dist/leaflet.css";
+  import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+  import icon from "leaflet/dist/images/marker-icon.png";
+  import iconShadow from "leaflet/dist/images/marker-shadow.png";
   import { onMount } from "svelte";
 
   export let elements: Validation.Element[];
 
   onMount(async () => {
     const L = await import("leaflet");
+    L.Marker.prototype.options.icon = L.icon({
+      iconRetinaUrl: iconRetina,
+      iconUrl: icon,
+      shadowUrl: iconShadow,
+      iconSize: [24,36],
+      iconAnchor: [12,36]
+    });
     const map = L.map("map").setView([35.39, 139.44], 6);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 18,
