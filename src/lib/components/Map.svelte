7ea56fd78ae35dd4739d49f9b6e93cf554e5a201
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-11-23 02:09:47 +0900

    地点をクリックした際に画像を表示する

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -8,6 +8,20 @@
 
   export let locations: Validation.Location[];
 
+  let locationImageUrl = "";
+  let locationName = "";
+
+  const getAttractionName = (element: Validation.Element) => {
+    if (element.tags["name:ja"] !== undefined) {
+      return element.tags["name:ja"];
+    } else if (element.tags.name !== undefined) {
+      return element.tags.name;
+    } else if (element.tags["name:en"] !== undefined) {
+      return element.tags["name:en"];
+    } else {
+      return element.title;
+    }
+  };
   onMount(async () => {
     const L = await import("leaflet");
     L.Marker.prototype.options.icon = L.icon({
@@ -22,6 +36,7 @@
       maxZoom: 18,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      className: "map-tiles",
     }).addTo(map);
     locations.forEach((location) => {
       const element = Object.values(location)[0];
@@ -30,25 +45,55 @@
       } as MarkerOptions)
         .addTo(map)
         .bindTooltip(
-          `<h1 class="my-2">${element.title}</h1><img src="${element.thumbnailUrl}"/>`,
+          `<h1 class="my-2">
+            ${getAttractionName(element)}
+          </h1><img src="${element.thumbnailUrl}"/>`,
         )
         .on("click", function (this: Marker) {
           const options = this.options as L.MarkerOptions;
           const element = Object.values(locations[options.id])[0];
-          console.log(element.url);
+          locationImageUrl = element.url!;
+          locationName = getAttractionName(element)!;
+          (
+            document.getElementById("location-detail")! as HTMLFormElement
+          ).showModal();
         });
     });
   });
 </script>
 
-<div id="map" class="w-[100vw] h-[100vh]" />
+<div id="map" class="w-[100vw] h-[100vh]">
+  <dialog id="location-detail" class="modal">
+    <div class="modal-box">
+      <h3 class="font-bold text-lg">{locationName}</h3>
+      {#key locationImageUrl}
+        <img src={locationImageUrl} alt="" />
+      {/key}
+      <p class="py-4">ここに説明が入ります</p>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+      <button>閉じる</button>
+    </form>
+  </dialog>
+</div>
 
 <style>
+  :global(.leaflet-control-attribution) {
+    display: none;
+  }
   :global(.leaflet-tooltip) {
     width: 20vw;
     white-space: normal;
     display: flex;
     flex-direction: column;
     justify-content: center;
+  }
+  :global(
+      .leaflet-layer,
+      .leaflet-control-zoom-in,
+      .leaflet-control-zoom-out,
+      .leaflet-control-attribution
+    ) {
+    filter: invert(90%) hue-rotate(180deg) brightness(100%) contrast(90%);
   }
 </style>
